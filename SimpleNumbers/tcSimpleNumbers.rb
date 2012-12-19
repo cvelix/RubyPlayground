8166aa88503bbf95fb06cf35458a281015884a11
Nenad Cvetkovic <cvelix@gmail.com>
2012-12-19 14:27:25 +0100

    Added new test cases

--- SimpleNumbers/tcSimpleNumbers.rb
+++ SimpleNumbers/tcSimpleNumbers.rb
@@ -1,6 +1,6 @@
 # File:  tcSimpleNumber.rb
  
-require_relative "SimpleNumbers"
+require "./SimpleNumbers"
 require "test/unit"
  
 class TestSimpleNumber < Test::Unit::TestCase
@@ -9,5 +9,12 @@ class TestSimpleNumber < Test::Unit::TestCase
     assert_equal(4, SimpleNumbers.new(2).add(2) )
     assert_equal(6, SimpleNumbers.new(2).multiply(3) )
   end
+  
+ def test_typecheck
+    assert_raise( RuntimeError ) { SimpleNumbers.new('a') }
+  end
  
+  def test_failure
+    assert_equal(3, SimpleNumbers.new(2).add(2), "Adding doesn't work" )
+  end
 end
