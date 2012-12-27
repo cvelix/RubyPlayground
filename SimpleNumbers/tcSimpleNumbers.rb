3594b69b331f29734b64bbe55fa4d4f68268869d
Nenad Cvetkovic <cvelix@gmail.com>
2012-12-27 10:12:49 +0100

    Sudoku and Text Analyzer

--- SimpleNumbers/tcSimpleNumbers.rb
+++ SimpleNumbers/tcSimpleNumbers.rb
@@ -5,9 +5,17 @@ require "test/unit"
  
 class TestSimpleNumber < Test::Unit::TestCase
  
+   def setup
+    @num = SimpleNumbers.new(2)
+  end
+ 
+  def teardown
+    ## Nothing really
+  end
+  
   def test_simple
-    assert_equal(4, SimpleNumbers.new(2).add(2) )
-    assert_equal(6, SimpleNumbers.new(2).multiply(3) )
+    assert_equal(4, @num.add(2) )
+    assert_equal(6, @num.multiply(3) )
   end
   
  def test_typecheck
@@ -15,6 +23,6 @@ class TestSimpleNumber < Test::Unit::TestCase
   end
  
   def test_failure
-    assert_equal(3, SimpleNumbers.new(2).add(2), "Adding doesn't work" )
+    assert_equal(3, @num.add(2), "Adding doesn't work" )
   end
 end
